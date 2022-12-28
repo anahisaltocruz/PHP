557d488396424663b5e6b5caec9ecabb23b5bfa2
Anahi Salto <asaltoc1600lumno.ipn.mx>
2022-12-27 18:20:01 -0600

    Ejemplo ed cómo usar el switch-case

--- IntroduccionPHP_inicio/12-condicionales.php
+++ IntroduccionPHP_inicio/12-condicionales.php
@@ -6,8 +6,25 @@
     if($autenticacion || $admin){
         //también se puede usar el operador and (&&)
         //Para negar se usa el signo de exclamación ( ! ) al inicio
+        //El else if funciona igual que en los otros lenguajes
+        //También los if anidados funcionan de una misma forma
         echo "Usuario autenticado correctamente";
     }else{
         echo "Usuario no ha sido autenticado correctamente";
     }
+
+    //Switches
+    $tecnologia = 'PHP';
+    switch($tecnologia){
+        case 'PHP':
+            echo "Aqui estoy en PHP";
+            break;
+        case 'C':
+            echo "Aqui estoy en C";
+            break;
+        default:
+            echo "Aqui estoy en cualquier otro lenguaje";
+            break;
+    }
+
 include 'includes/footer.php';
