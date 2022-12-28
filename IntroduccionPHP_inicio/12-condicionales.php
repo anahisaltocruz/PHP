9642a43bb6e92f17fb215a6595e5edfc69b79ee3
Anahi Salto <asaltoc1600lumno.ipn.mx>
2022-12-27 18:13:05 -0600

    Tema 12: Condicionales, if, else,...

--- IntroduccionPHP_inicio/12-condicionales.php
+++ IntroduccionPHP_inicio/12-condicionales.php
@@ -1,7 +1,13 @@
 <?php include 'includes/header.php';
     //Condicionales en php
+    $autenticacion = true;
+    $admin = false;
 
-
-
-
+    if($autenticacion || $admin){
+        //también se puede usar el operador and (&&)
+        //Para negar se usa el signo de exclamación ( ! ) al inicio
+        echo "Usuario autenticado correctamente";
+    }else{
+        echo "Usuario no ha sido autenticado correctamente";
+    }
 include 'includes/footer.php';
