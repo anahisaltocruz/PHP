cf5f071c0b02a2ba8dd5fbda26549fa320c2236f
Anahi Salto <asaltoc1600lumno.ipn.mx>
2022-12-28 17:09:54 -0600

    Ejemplos de json -> Convertir un array a string y viceversa

--- IntroduccionPHP_inicio/17-require.php
+++ IntroduccionPHP_inicio/17-require.php
@@ -8,4 +8,18 @@
      * de forma "normal"
      */
 
+     /**
+      * El include que aparece en este dicumento al inicio, nos sirve para
+      * poder asignar el mismo encabezado a todos los archivos que hemos hecho
+      * en PHP
+
+      * require funciona igual que la palabra include
+      * require se recomienda para funciones "importantes"
+      * include se recomienda para llamar funciones que no son vitales
+      * para el funcionamiento de la aplicación
+
+      * require_once : funciona <casi> igual que require, solo que primero revisa
+      * si ya ha sido agregado, si no, lo incluye, si sí, solo se ignora esa linea
+      */
+
 include 'includes/footer.php';
--- IntroduccionPHP_inicio/18.json.php
+++ IntroduccionPHP_inicio/18.json.php
@@ -1,8 +1,36 @@
 <?php include 'includes/header.php';
 
-
-
-
-
+/**
+ * Los objetos en JS son equivalentes a los arreglos asociativos
+ */
+$productos = [
+    [   'nombre' => 'Tablet',
+        'precio' => 200,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Televisión 24"',
+        'precio' => 300,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Monitor Curvo',
+        'precio' => 400,
+        'disponible' => false
+    ]
+];
+echo "<pre>";
+    var_dump($productos);
+    //Lo que hace esta funcion es convertir un array a string
+    $json = json_encode($productos,JSON_UNESCAPED_UNICODE);//El segundo parametro nos sirve para que tome
+                                                            //en cuenta correctamente los acentos
+    $json_array = json_decode($json);//Convierte un string a un array                                                            
+    echo "<br>";
+    echo "<br>";
+    var_dump($json);
+    echo "<br>";
+    echo "<br>";
+    var_dump($json_array);
+echo "</pre>";
 
 include 'includes/footer.php';
