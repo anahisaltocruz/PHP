24124d902f370c6dccfcf5ba544f86cdb586e076
Anahi Salto <asaltoc1600lumno.ipn.mx>
2022-12-27 21:17:54 -0600

    Intercambiar el orden de los parametros en las funciones en PHP

--- IntroduccionPHP_inicio/17-require.php
+++ IntroduccionPHP_inicio/17-require.php
@@ -1,8 +1,11 @@
 <?php include 'includes/header.php';
-
-
-
-
-
+    /**
+     * Si ponemos el nombre dentro de los argumentos de la función
+     * cuando la llamamos, podemos cambiar el orden de cómo vamos 
+     * pasando los parametros
+     * 
+     * La función trabaja completamente igual a que si lo pasamos 
+     * de forma "normal"
+     */
 
 include 'includes/footer.php';
